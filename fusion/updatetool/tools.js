7f48ba7ce8d0961ccd706f322483345849afcb43
PizzaBlu <pizzabluow@gmail.com>
2026-01-27 21:01:11 -0500

    add update tool

---
+++ fusion/updatetool/tools.js
@@ -0,0 +1,17 @@
+export const TOOLS = {
+    // effects
+    posterize: {name: "Posterize", release: 1, version: '1.0'},
+    halftone: {name: "Halftone", release: 1, version: '1.0'},
+    long_shadow: {name: "LongShadow", release: 1, version: '1.0'},
+    neon_glow: {name: "NeonGlow", release: 1, version: '1.0'},
+    outline: {name: "Outline", release: 1, version: '1.0'},
+
+    // masks
+    mask_wipe: {name: "MaskWipe", release: 1, version: '1.0'},
+    squircle: {name: "Squircle", release: 1, version: '1.0'},
+    
+    // modifiers
+    time_seconds: {name: "Time/Seconds", release: 1, version: '1.0'},
+    time_frames: {name: "Time/Frames", release: 1, version: '1.0'},
+    time_duration: {name: "Time/Duration", release: 1, version: '1.0'},
+}
